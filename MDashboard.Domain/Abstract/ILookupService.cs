5493bb06605328d51f8b46cb5928b446d7741eb5
Tinashe <tinjiri@gmail.com>
2020-05-10 07:38:33 +0200

    Added lookupservice

---
+++ MDashboard.Domain/Abstract/ILookupService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MDashboard.Domain.Entities;
+
+namespace MDashboard.Domain.Abstract
+{
+   public interface  ILookupService
+    {
+        IQueryable<Title> GetTitles();
+    }
+}
---
+++ MDashboard.Services/LookupService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MDashboard.DataAccess;
+using MDashboard.Domain.Abstract;
+using MDashboard.Domain.Entities;
+
+namespace MDashboard.Services
+{
+    public class LookupService : ILookupService
+    {
+        private DashboardContext context;
+        public LookupService(DashboardContext context)
+        {
+            this.context = context;
+        }
+        public IQueryable<Title> GetTitles()
+        {
+            //throw new NotImplementedException();
+            return context.Titles;
+        }
+    }
+}
