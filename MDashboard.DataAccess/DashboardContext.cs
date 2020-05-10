bb84919308b05196443c6a105a699a7115ca3c66
Tinashe <tinjiri@gmail.com>
2020-05-10 07:40:44 +0200

    Added DataAccess Packages

---
+++ MDashboard.DataAccess/DashboardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MDashboard.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MDashboard.DataAccess
+{
+    public class DashboardContext : DbContext
+    {
+        public DashboardContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Title> Titles { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Title>().ToTable("Title");
+        }
+    }
+}
