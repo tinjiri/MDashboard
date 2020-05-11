b53c36e7e44a7316d97866ec40465b137fc6b101
Tinashe <tinjiri@gmail.com>
2020-05-11 11:08:46 +0200

    Added JobController

---
+++ MDashboard.WebAPI/Controllers/JobController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MDashboard.WebAPI.Controllers
+{
+    /// <summary>
+    /// Job api to retrieve the jobs/tasks used by dashboard.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+    }
+}
