18632977fed43308bf0cc7e274f4d8ed26bebcfe
Tinashe <tinjiri@gmail.com>
2020-07-07 10:26:20 +0200

    fixed the swagger configuration

--- MDashboard.WebAPI/Startup.cs
+++ MDashboard.WebAPI/Startup.cs
@@ -16,15 +16,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
 
 namespace MDashboard.WebAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IHostingEnvironment env { get; }
+        public Startup(IHostingEnvironment env, IConfiguration configuration)
         {
             Configuration = configuration;
+            this.env = env;
         }
+       
 
         public IConfiguration Configuration { get; }
 
@@ -41,18 +46,17 @@ namespace MDashboard.WebAPI
               );
             });
 
-            services.AddSwaggerGen(options=> {
-                options.SwaggerDoc("v1",
-                    new Microsoft.OpenApi.Models.OpenApiInfo
-                    {
-                        Title = "Dashboard API",
-                        Description = "Show Dashboard API",
-                        Version = "v1"
-                    });
-                var fileName =$"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var filePath = Path.Combine(AppContext.BaseDirectory,fileName);
-                options.IncludeXmlComments(filePath);
-            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var pathIncludeXmlComments =
+                $@"{env.ContentRootPath}\{env.ApplicationName}.xml";
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Dashboard API", Version = "v1" });
+
+                if (System.IO.File.Exists(pathIncludeXmlComments))
+                    c.IncludeXmlComments(pathIncludeXmlComments);
+            });           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
